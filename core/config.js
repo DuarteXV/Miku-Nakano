3ed6183b3ce5764a4b61f68d286efc1273b2af63
DuarteXV <danielalejandrobasado@gmail.com>
2026-04-21 21:33:46 -0500

    Crear core/config.js

---
+++ core/config.js
@@ -0,0 +1,9 @@
+global.prefix = '/';
+global.botName = 'WaBot';
+global.owners = ['50498273976', '573135180876'];
+
+global.channelJid = '120363408940496562@newsletter';
+global.channelName = 'Ado 🐢';
+global.channelLink = 'https://whatsapp.com/channel/0029VbDFqTQ60eBcg7bKSb38';
+
+export default global;
