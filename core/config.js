f45c2c2bcc1b578d83fc3a7907cef1ec12d5e9ce
DuarteXV <danielalejandrobasado@gmail.com>
2026-04-25 18:15:20 -0500

    Delete core directory

--- core/config.js
+++
@@ -1,9 +0,0 @@
-global.prefix = '/';
-global.botName = 'Miku Nakano';
-global.owners = ['50498273976', '573135180876'];
-
-global.channelJid = '120363408940496562@newsletter';
-global.channelName = 'Ado 🐢';
-global.channelLink = 'https://whatsapp.com/channel/0029VbDFqTQ60eBcg7bKSb38';
-
-export default global;
